a2b965f231c2f21a3a6d96fe9321cdaaa05e7462
Russbell Noreña <92810561+russbelln@users.noreply.github.com>
2023-08-28 16:17:23 -0500

    Create mi_primer_app.py

---
+++ mi_primer_app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+# Título y autor de la app
+st.title("Mi primera app")
+st.write("Esta app fue elaborada por “RUSSBELL NOREÑA MEJIA”.")
+
+# Pregunta al usuario por su nombre
+nombre_usuario = st.text_input("Por favor, ingresa tu nombre")
+
+# Verifica si el usuario ingresó un nombre
+if nombre_usuario:
+    mensaje_bienvenida = f"{nombre_usuario}, te doy la bienvenida a mi primera app."
+    st.write(mensaje_bienvenida)
